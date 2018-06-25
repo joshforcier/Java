fe658a6875f6a172b9e2024a9c9ba9368fea1e8f
joshforcier <joshforcier@gmail.com>
2018-06-25 14:30:39 -0500

    Create findPair.java

---
+++ Arrays/findPair.java
@@ -0,0 +1,29 @@
+//Given an unsorted array of integers, find a pair with given sum in it.
+
+public class findPair 
+{	
+	public static void findPair(int[] x, int sum) 
+	{		
+		for (int i = 0; i < x.length -1; i++) 
+		{			
+			for (int j = 0; j < x.length; j++) 
+			{				
+				if (x[i] + x[j] == sum) 
+				{					
+					System.out.println("Found pair at " + i + " and " + j);
+					
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+		
+		int x[] = {8, 5 , 4, 2, 7, 9, 1};
+		int sum = 10;
+		
+		findPair(x, sum);		
+
+	}
+}
